a8c0086dd3db7c49f9ffe519bbb9def6dc580d6e
git-volt.software <info@volt.software>
2022-12-05 21:08:42 -0500

    finished exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //ask user for favorite subject
+            Console.WriteLine("What is your favorite school subject?");
+            string subject = Console.ReadLine();
+
+            
+            
+            switch (subject)
+            {
+                case "Science":
+                    Console.WriteLine("Man I hate Science.");
+                    break;
+                case "Math":
+                    Console.WriteLine("Man I hate Math.");
+                    break;
+                case "English":
+                    Console.WriteLine("Man I hate English.");
+                    break;
+                case "History":
+                    Console.WriteLine("Man I hate History.");
+                    break;
+                case "Computer Science":
+                    Console.WriteLine("Man I love Computer Science.");
+                    break;
+                default:
+                    Console.WriteLine("School is for fools!");
+                    break;
+            }
+
         }
     }
 }
